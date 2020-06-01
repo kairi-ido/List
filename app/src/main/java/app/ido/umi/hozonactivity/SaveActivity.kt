e47670e1027f0a74cc1579df7d9b468743ac207f
lifeistech <kaairiinn0913@yahoo.co.jp>
2020-06-02 04:39:14 +0900

    フローティングアクションボタン追加＋リスト追加未完成

---
+++ app/src/main/java/app/ido/umi/hozonactivity/SaveActivity.kt
@@ -0,0 +1,58 @@
+package app.ido.umi.hozonactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.system.Os.read
+import io.realm.Realm
+import io.realm.RealmResults
+import io.realm.Sort
+import kotlinx.android.synthetic.main.activity_save.*
+import kotlinx.android.synthetic.main.list_item.*
+import java.util.*
+
+class SaveActivity : AppCompatActivity() {
+
+    private val realm: Realm by lazy {
+        Realm.getDefaultInstance()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_save)
+
+        backButton.setOnClickListener {
+            finish()
+        }
+
+        hozonButton.setOnClickListener {
+            val name:String = nameEditText.text.toString()
+            create(name)
+        }
+
+    }
+
+
+
+
+
+
+    fun create(name:String) {
+
+            realm.executeTransaction {
+                val task = it.createObject(Task::class.java, UUID.randomUUID().toString())
+
+                task.name = name
+
+
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+
