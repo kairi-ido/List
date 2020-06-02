37c7a366aa1cbadb85a04c1e6f7bddd5a914be50
lifeistech <kaairiinn0913@yahoo.co.jp>
2020-06-02 13:02:00 +0900

    リストに新しいリストを作り文字を追加できた！！！！

--- app/src/main/java/app/ido/umi/hozonactivity/SaveActivity.kt
+++ app/src/main/java/app/ido/umi/hozonactivity/SaveActivity.kt
@@ -24,6 +24,7 @@ class SaveActivity : AppCompatActivity() {
             finish()
         }
 
+
         hozonButton.setOnClickListener {
             val name:String = nameEditText.text.toString()
             create(name)
--- app/src/main/java/app/ido/umi/hozonactivity/TaskAdapter.kt
+++ app/src/main/java/app/ido/umi/hozonactivity/TaskAdapter.kt
@@ -32,7 +32,7 @@ class TaskAdapter(
         }
 
         holder.imageView.setImageResource(task.imageId)
-        holder.contentTextView.text = task.content
+        holder.contentTextView.text = task.name
         holder.dateTextView.text =
             SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.JAPANESE).format(task.createdAt)
 
